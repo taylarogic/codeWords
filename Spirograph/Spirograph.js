cf82bc6be62042d6e96418093e5d9600f27de447
Tayla Rogic <68985112+taylarogic@users.noreply.github.com>
2020-08-22 17:10:52 +1000

    Add files via upload

---
+++ Spirograph/Spirograph.js
@@ -0,0 +1,28 @@
+
+var spin=0;
+
+function setup() {
+  createCanvas(600,600);
+  frameRate(15);
+}
+
+function draw() {
+  translate(300,300);
+  rotate(spin);
+  spin=spin+3;
+  
+  stroke(0);
+  strokeWeight(1);
+  fill(240,0);
+  rect(100,100,500,500);
+  stroke(255,0,0);
+  rect(100,200,500,500);
+  stroke(255,0,255);
+  rect(100,0,500,500);
+  stroke(0);
+  rect(100,-100,500,500);
+  stroke(255,0,0);
+  rect(100,-200,500,500);
+  
+  
+}
